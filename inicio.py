6ede817b836a2a59dae0958982d50a96f3e980d9
Djaysel Pessôa <djaysel@outlook.com>
2021-08-26 14:29:06 -0300

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -6,6 +6,7 @@ if emprestimo.find(","):
     print(emprestimoFinal)
 else:
     emprestimoFinal = float(emprestimo)
+    print(emprestimoFinal)
 
 if 2000 <= emprestimoFinal <= 10000:
     # juros composto
@@ -13,55 +14,60 @@ if 2000 <= emprestimoFinal <= 10000:
     while active:
         try:
             escolha = input("Para valores entre 2000 e 10000 você poderá escolher entre 7 e 16 prestações: ")
-            escolha = float(escolha)
+            escolha = int(escolha)
             if not 7 <= escolha <= 16:
                 raise ValueError("Valor incompatível!")
             else:
-                parcelas = escolha
+                parcelas = float(escolha)
                 break
         except ValueError as e:
             print("!erro!", e)
-    calc = emprestimoFinal * ((1 + 0.015) ** escolha)
-    calc = round(calc, 2)
-
-    juros = ((1 + 0.015) ** escolha)
-    juros = round(juros, 2)
+    # calc = emprestimoFinal * ((1 + 0.015) ** escolha)
+    # calc = round(calc, 2)
+    
     emprestimoParcela = emprestimoFinal/parcelas
     emprestimoParcela = round(emprestimoParcela, 2)
-    jurosParcelaFinal = calc / parcelas
-    jurosParcelaFinal = jurosParcelaFinal + emprestimoParcela
-    jurosParcelaFinal = round(jurosParcelaFinal, 2)
-    valorFinal = emprestimoFinal + calc
-    valorFinal = round(valorFinal, 2)
+    totalParcelas = 0.0
+    calc = emprestimoParcela
+    print(f"Você solicitou {escolha} parcelas!")
+    for i in range(escolha):
+        cont = i + 1
+        calc = calc + (calc * 0.015)
+        calc = round(calc, 2)
+        totalParcelas = totalParcelas + calc
+        print(f"No {cont}º mês você pagará o valor de R$ {calc}")
+        i += 1
 
-    print(f"Você pagará {parcelas} x de R$ {emprestimoParcela} mais juros composto de {juros}% ao mês resultando em uma adicional de R$ {calc}.")
-    print(f"O valor final a pagar é de R$ {valorFinal}. Cada parcela com juros fica no valor de R$ {jurosParcelaFinal}")
+    valorFinalJuros = totalParcelas - emprestimoFinal
+    valorFinalJuros = round(valorFinalJuros, 2)
+    totalParcelas = round(totalParcelas, 2)
+
+    print(f"Valor solicitado: R$ {emprestimoFinal}\nValor final total com juros composto: R$ {totalParcelas}\nValor total dos juros adicionado: R$ {valorFinalJuros}\n")
+ 
 elif 250 <= emprestimoFinal <= 2000:
     # juros simples
     active = True
     while active:
         try:
             escolha = input("Para valores entre 250 e 2000 você poderá escolher entre 3 e 6 prestações: ")
-            escolha = float(escolha)
+            escolha = int(escolha)
             if not 3 <= escolha <= 6:
                 raise ValueError("Valor incompatível!")
             else:
-                parcelas = escolha
+                parcelas = float(escolha)
                 break
         except ValueError as e:
             print("!erro!", e)
-    calc = emprestimoFinal * 0.12 * escolha
+    calc = emprestimoFinal * 0.12 * parcelas
     calc = round(calc, 2)
-    emprestimoParcela = emprestimoFinal/parcelas
-    emprestimoParcela = round(emprestimoParcela, 2)
-    jurosParcelaFinal2 = calc / parcelas
-    jurosParcelaFinal2 = jurosParcelaFinal2 + emprestimoParcela
-    jurosParcelaFinal2 = round(jurosParcelaFinal2, 2)
-    valorFinal = emprestimoFinal + calc
-    valorFinal = round(valorFinal, 2)
+    valorFinalJuros = emprestimoFinal + calc
+    valorFinalJuros = round(valorFinalJuros, 2)
 
-    print(f"Você pagará {parcelas} x de R$ {emprestimoParcela} mais juros simples de 12% resultando em um adicional de R$ {calc}.")
-    print(f"O valor final total é de R$ {valorFinal}. O valor com juros de cada parcela fica: R$ {jurosParcelaFinal2}")
+    valorParcela = (emprestimoFinal / parcelas) + (calc / parcelas)
+    valorParcela = round(valorParcela, 2)
+    
+    print(f"Valor solicitado: R$ {emprestimoFinal}\nValor final total com juros simples: R$ {valorFinalJuros}\nValor total dos juros adicionado: R$ {calc}\nValor de cada uma das {escolha} parcelas: R$ {valorParcela}")
+
 else:
     active = True
     while active:
